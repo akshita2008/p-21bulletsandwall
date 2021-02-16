d89ea4fde936027d407d67a22a6226855f5fe82b
akshita2008 <75514912+akshita2008@users.noreply.github.com>
2021-02-16 19:54:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var wall , bullet;
+var speed, weight;
+var thickness;
+
+function setup() {
+  createCanvas(1600,400);
+  createSprite(400, 200, 50, 50);
+  bullet=createSprite(50,200,50,20);
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor= color(80,80,80);
+speed=random(223,321);
+weight=random(30,52);
+thickness=random(22,83);
+bullet.velocityX= speed;
+}
+
+function draw() {
+  background(255,255,255);  
+  if(wall.x-bullet.x < (bullet.width+wall.width)/2){
+    bullet.velocityX=0;
+    var deformation = 0.5 * weight * speed *speed /(thickness*thickness*thickness);
+    console.log(deformation);
+
+    if (deformation >= 10){
+      bullet.shapeColor= "red";
+    }
+
+    
+
+    if(deformation<10){
+      bullet.shapeColor= "green";
+    }
+   
+    
+
+  }
+  
+
+  drawSprites();
+}
